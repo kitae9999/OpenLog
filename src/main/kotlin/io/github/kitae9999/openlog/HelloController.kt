efee7b927a4c318541723b9a56c1677f88a6c1a6
kitae <gitae9999@naver.com>
2026-02-27 00:13:21 +0900

    setting: 개발환경세팅

--- src/main/kotlin/io/github/kitae9999/openlog/HelloController.kt
+++ src/main/kotlin/io/github/kitae9999/openlog/HelloController.kt
@@ -1,4 +1,12 @@
 package io.github.kitae9999.openlog
 
-class HelloControoler {
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    fun hello() : String {
+        return "hello, world!"
+    }
 }
