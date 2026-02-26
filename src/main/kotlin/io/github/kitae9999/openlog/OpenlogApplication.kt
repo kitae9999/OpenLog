3641dcb1a135299da29ba568286f4960205b5255
kitae <gitae9999@naver.com>
2026-02-27 00:11:21 +0900

    Stop tracking local config

---
+++ src/main/kotlin/io/github/kitae9999/openlog/OpenlogApplication.kt
@@ -0,0 +1,11 @@
+package io.github.kitae9999.openlog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenlogApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpenlogApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/kitae9999/openlog/OpenlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.kitae9999.openlog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenlogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
