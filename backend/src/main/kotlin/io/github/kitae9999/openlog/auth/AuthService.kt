0f0cbca80e84cbc834311534f6773107559b4b21
kitae <gitae9999@naver.com>
2026-03-17 13:55:39 +0900

    feat: google Oauth 서비스 메서드 추가

--- backend/src/main/kotlin/io/github/kitae9999/openlog/auth/AuthService.kt
+++ backend/src/main/kotlin/io/github/kitae9999/openlog/auth/AuthService.kt
@@ -1,8 +1,11 @@
 package io.github.kitae9999.openlog.auth
 
+import io.github.kitae9999.openlog.auth.exception.InvalidOAuthStateException
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
 import org.springframework.web.util.UriComponentsBuilder
+import java.time.Duration
 import java.util.UUID
 
 @Service
@@ -11,25 +14,64 @@ class AuthService(
     private val clientId : String,
     @Value("\${oauth.google.redirect-uri}")
     private val redirectUri : String,
+    private val redisTemplate: StringRedisTemplate,
 ) {
-    /**
-     * UUID State 생성
-     * */
-    fun generateState(): String = UUID.randomUUID().toString()
+    companion object {
+        private  val OAUTH_STATE_TTL: Duration = Duration.ofMinutes(5)
+        private const val GOOGLE_STATE_KEY_PREFIX = "oauth:google:state"
+    }
 
+    data class GoogleAuthRequest(
+        val flowId: String,
+        val authUrl: String,
+    )
+
+    /**
+     * auth 이벤트 발생 시 state 발행 및 레디스 저장
+     */
+    fun createGoogleAuthRequest(): GoogleAuthRequest {
+        val flowId = UUID.randomUUID().toString()
+        val state = UUID.randomUUID().toString()
+
+        redisTemplate.opsForValue().set(
+            buildStateKey(flowId),
+            state,
+            OAUTH_STATE_TTL
+        )
+
+        return GoogleAuthRequest(
+            flowId = flowId,
+            authUrl = getGoogleAuthUrl(state),
+        )
+
+    }
+
+
+    fun validateGoogleState(flowId: String?, state: String){
+        if (flowId.isNullOrBlank()){
+            throw InvalidOAuthStateException()
+        }
+
+        val savedState = redisTemplate.opsForValue().getAndDelete(buildStateKey(flowId))
+        if (savedState == null || savedState != state ){
+            throw InvalidOAuthStateException()
+        }
+    }
 
     /**
      * 구글 로그인 페이지 이동 URL 생성
      */
-    fun getGoogleAuthUrl(): String = UriComponentsBuilder
+    private fun getGoogleAuthUrl(state: String): String = UriComponentsBuilder
             .fromUriString("https://accounts.google.com/o/oauth2/v2/auth")
             .queryParam("client_id", clientId)
             .queryParam("redirect_uri", redirectUri)
             .queryParam("response_type", "code")
             .queryParam("scope", "openid email profile")
+            .queryParam("state",state)
             .build()
             .encode()
             .toUriString()
 
-
+    private fun buildStateKey(flowId: String): String =
+        "$GOOGLE_STATE_KEY_PREFIX$flowId"
 }
