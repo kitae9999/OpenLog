6cb86dfd12dbe9b8ab4b096919904aac9f239716
kitae <gitae9999@naver.com>
2026-03-22 02:09:30 +0900

    feat: 유저 레포지토리

---
+++ backend/src/main/kotlin/io/github/kitae9999/openlog/user/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package io.github.kitae9999.openlog.user.repository
+
+import io.github.kitae9999.openlog.user.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository: JpaRepository<User, Long> {
+}
