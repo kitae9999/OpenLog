b572614450ea83681b690bf268786e1b277b0a38
kitae <gitae9999@naver.com>
2026-04-02 15:43:54 +0900

    chore: 인증상태 캐시 stale,gctime ㅈ조ㅓㅇ

--- frontend/src/02_features/auth/model/useAuthState.ts
+++ frontend/src/02_features/auth/model/useAuthState.ts
@@ -2,10 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import { getUser } from "@/features/auth/api/getUser";
 
 export const useAuthState = () => {
-  const { data } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["authState"],
     queryFn: getUser,
+    staleTime: 1000 * 5 * 60,
+    gcTime: 1000 * 30 * 60,
   });
 
-  return data;
+  return {
+    data,
+    isLoading,
+    isError,
+  };
 };
