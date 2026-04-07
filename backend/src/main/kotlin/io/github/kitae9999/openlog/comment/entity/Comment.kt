963b18c6ef30ef62244958977d693fd9876dd258
kitae <gitae9999@naver.com>
2026-04-07 16:32:34 +0900

    feat: Comment 엔티티 정의

---
+++ backend/src/main/kotlin/io/github/kitae9999/openlog/comment/entity/Comment.kt
@@ -0,0 +1,51 @@
+package io.github.kitae9999.openlog.comment.entity
+
+import io.github.kitae9999.openlog.post.entity.Post
+import io.github.kitae9999.openlog.user.entity.User
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(
+    name = "comments"
+)
+class Comment (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id : Long? = null,
+    post: Post,
+    user : User,
+    content: String
+){
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    var post : Post = post
+        protected set
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    var user : User = user
+        protected set
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    var content: String = content
+        protected set
+
+    @Column(name = "created_at", nullable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+        protected set
+
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+        protected set
+
+
+}
