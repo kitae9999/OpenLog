65327ab2c9a0bf51fbfd962954e126a442f8d1c1
kitae <gitae9999@naver.com>
2026-05-01 17:07:02 +0900

    feat: 프론트 깃허브 로그인 주소 연결

--- frontend/src/02_features/auth/api/handleOAuth.ts
+++ frontend/src/02_features/auth/api/handleOAuth.ts
@@ -1,9 +1,9 @@
 import { API_CONFIG } from "@/shared/api";
 import type { OAuthProvider } from "@/features/auth/model/auth.type";
 
-const providerPath = {
-  GOOGLE: "google",
-  GITHUB: "github",
+const providerOAuthPath = {
+  GOOGLE: "/api/auth/google",
+  GITHUB: "/api/oauth2/authorization/github",
 } satisfies Record<OAuthProvider, string>;
 
 export const handleOAuth = (provider: OAuthProvider) => {
@@ -12,7 +12,7 @@ export const handleOAuth = (provider: OAuthProvider) => {
   }
 
   const endpoint = new URL(
-    `/api/auth/${providerPath[provider]}`,
+    providerOAuthPath[provider],
     API_CONFIG.baseURL,
   );
 
