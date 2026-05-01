7b457a4fc84da105c5564448d11e313d724e2114
kitae <gitae9999@naver.com>
2026-05-01 20:47:39 +0900

    feat: Kafka EventPublusher 구현체

---
+++ backend/src/main/kotlin/io/github/kitae9999/openlog/common/event/KafkaEventPublisher.kt
@@ -0,0 +1,13 @@
+package io.github.kitae9999.openlog.common.event
+
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class KafkaEventPublisher(
+    private val kafkaTemplate: KafkaTemplate<String, Any>
+): EventPublisher {
+    override fun <T : Any> publish(topic: String, key: String, event: T) {
+        kafkaTemplate.send(topic, key, event) // Topic, Key, Value
+    }
+}
