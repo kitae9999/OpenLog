75ff34a3ef9b3260bee002befb3e0c41f9b5b689
kitae <gitae9999@naver.com>
2026-07-07 00:02:23 +0900

    refactor: workspace 하위경로 url prefix 추가

--- backend/src/main/kotlin/io/github/kitae9999/openlog/workspace/WorkspaceLinkController.kt
+++ backend/src/main/kotlin/io/github/kitae9999/openlog/workspace/WorkspaceLinkController.kt
@@ -1,12 +1,23 @@
 package io.github.kitae9999.openlog.workspace
 
+import io.github.kitae9999.openlog.user.entity.User
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class WorkspaceLinkController (
-    private val workspaceLinkService: WorkspaceLinkService
+@RequestMapping("/workspaces")
+class WorkspaceLinkController(
+    private val workspaceLinkService: WorkspaceLinkService,
 ) {
-
+    @PostMapping("/{workspaceId}/log-links")
+    fun createLogLink(
+        @AuthenticationPrincipal user: User,
+        @PathVariable workspaceId: Long,
+    ): ResponseEntity<Void> {
+        TODO("createLogLink 구현 필요")
+    }
 }
--- backend/src/main/kotlin/io/github/kitae9999/openlog/workspace/WorkspaceLogController.kt
+++ backend/src/main/kotlin/io/github/kitae9999/openlog/workspace/WorkspaceLogController.kt
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping
+@RequestMapping("/workspaces")
 class WorkspaceLogController(
     private val workspaceLogService: WorkspaceLogService,
 ) {
